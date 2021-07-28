1a0b0009f047ba14ee7bd5635e2eb902e3b00aea
Vinicius Souza <viniciuspodi@gmail.com>
2021-07-27 20:08:25 -0400

    Corrigindo lista de convidados v2

--- 05 - lista de convidados/v2/js/listaConvidados.js
+++ 05 - lista de convidados/v2/js/listaConvidados.js
@@ -4,11 +4,9 @@ function adicionarALista() {
   let nome = document.getElementById("inputConvidado").value
 
   if (nome != "") {
-    // listaConvidados = listaConvidados + "<li>" + nome + "</li>"
+    listaConvidados = listaConvidados + nome + "\n"
 
-    listaConvidados = listaConvidados + `<li> ${nome} </li>`
-
-    document.getElementById("lista").innerHTML = listaConvidados
+    document.getElementById("lista").innerText = listaConvidados
     document.getElementById("inputConvidado").value = ""
   } else {
     alert("Preencha o nome do convidado!")
