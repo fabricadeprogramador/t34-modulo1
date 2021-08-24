c7de25e7d536fc894a8af4f092398720ea5f485e
Vinicius Souza <viniciuspodi@gmail.com>
2021-08-24 19:29:52 -0400

    Corrigindo calculadora de BTUS orientada a objetos

--- exercícios/calculoBTUS/js/calculadoraBTUS.js
+++ exercícios/calculoBTUS/js/calculadoraBTUS.js
@@ -1,22 +1,60 @@
-function calcularBTUS() {
-  let totalMetros = document.getElementById("totalArea").value
-  let totalPessoas = document.getElementById("totalPessoas").value
-  let totalEletro = document.getElementById("totalEletro").value
+class CalculadoraBTUS {
+  constructor() {}
 
-  if (totalMetros == "" || totalPessoas == "" || totalEletro == "") {
-    alert("Preencha todos os campos!")
-  } else {
-    let metrosConvertidos = parseFloat(totalMetros)
-    let pessoasConvertidos = parseInt(totalPessoas)
-    let eletroConvertidos = parseInt(totalEletro)
+  lerDadosDeEntrada() {
+    this.totalMetros = document.getElementById("totalArea").value
+    this.totalPessoas = document.getElementById("totalPessoas").value
+    this.totalEletro = document.getElementById("totalEletro").value
+  }
 
-    totalBTUS =
-      metrosConvertidos * 600 +
-      eletroConvertidos * 600 +
-      (pessoasConvertidos - 1) * 600
+  validarEConverter() {
+    if (
+      this.totalMetros == "" ||
+      this.totalPessoas == "" ||
+      this.totalEletro == ""
+    ) {
+      alert("Preecha todos os campos!")
+      return false
+    }
 
+    this.totalMetros = parseFloat(this.totalMetros)
+    this.totalPessoas = parseInt(this.totalPessoas)
+    this.totalEletro = parseInt(this.totalEletro)
+
+    return true
+  }
+
+  calcularBTUS() {
+    this.limparDadosSaida()
+    this.lerDadosDeEntrada()
+    let ehValido = this.validarEConverter()
+
+    if (ehValido == true) {
+      let totalBTUS =
+        this.totalMetros * 600 +
+        this.totalEletro * 600 +
+        (this.totalPessoas - 1) * 600
+
+      this.exibirResultado(totalBTUS)
+      this.limparDadosEntrada()
+    }
+  }
+
+  limparDadosEntrada() {
+    document.getElementById("totalArea").value = ""
+    document.getElementById("totalPessoas").value = ""
+    document.getElementById("totalEletro").value = ""
+  }
+
+  limparDadosSaida() {
+    document.getElementById("total").innerText = ""
+  }
+
+  exibirResultado(resultado) {
     document.getElementById(
       "total"
-    ).innerText = `É necessário um total de ${totalBTUS} BTUS neste cômodo!`
+    ).innerText = `É necessário um total de ${resultado} BTUS neste cômodo!`
   }
 }
+
+let calculadora = new CalculadoraBTUS()
